e5cd9beacebb4df907b3179e2bfbac0f865a3dfe
Emily Achieng <emillyachieng@gmail.com>
2018-01-25 17:32:08 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,14 +17,17 @@ api = tweepy.API(auth)
 
 def tweet_image(url, username, status_id):
     filename = 'temp.png'
-    # send a get request
+    
+    # get request
     request = requests.get(url, stream=True)
     if request.status_code == 200:
-        # read data from downloaded bytes and returns a PIL.Image.Image object
+        
         i = Image.open(BytesIO(request.content))
+        
         # Saves the image under the given filename
         i.save(filename)
         scramble(filename)
+        
         # Update the authenticated user’s status
         api.update_with_media('scramble.png', status='@{0}'.format(username), in_reply_to_status_id=status_id)
     else:
@@ -32,26 +35,28 @@ def tweet_image(url, username, status_id):
 
 
 def scramble(filename):
-    BLOCKLEN = 64  # Adjust and be careful here.
+    BLOCKLEN = 64  
 
     img = Image.open(filename)
     width, height = img.size
 
     xblock = width // BLOCKLEN
     yblock = height // BLOCKLEN
-    # creates sequence of 4-tuples (box) defining the left, upper, right, and lower pixel coordinate
+    
+    # creates sequence of 4-tuples (box) 
+    # defining the left, upper, right, and lower pixel coordinate
     blockmap = [(xb * BLOCKLEN, yb * BLOCKLEN, (xb + 1) * BLOCKLEN, (yb + 1) * BLOCKLEN)
                 for xb in range(xblock) for yb in range(yblock)]
 
     shuffle = list(blockmap)
 
-    # shuffle the sequence
+    # shuffle sequence
     random.shuffle(shuffle)
 
     # Creates a new image with the given mode and size.
     result = Image.new(img.mode, (width, height))
     for box, sbox in zip(blockmap, shuffle):
-        # Returns a rectangular region from this original image.
+        # Returns a rectangular region from  original image.
         crop = img.crop(sbox)
         # Pastes the cropped pixel into the new image Object
         result.paste(crop, box)
